be968b1a4e29f036efbc3b92d7cbdec76efb32e4
Mihai Galos <mihaigalos@gmail.com>
2019-08-04 09:55:56 +0200

    Add newlines after function calls

--- create_base_image.sh
+++ create_base_image.sh
@@ -10,16 +10,19 @@ function set_variables {
   EXTRACT_DIR=/home/osmc/Downloads/DockerImages
   MOUNT_DIR=/home/osmc/Downloads/DockerImagesMnt/
   LOOP_DEV=/dev/loop1 # Loop device used for mounting .img file
+  echo ""
 }
 
 function make_directories {
   mkdir -p "${DOWNLOAD_DIR}"
   mkdir -p "${EXTRACT_DIR}"
   mkdir -p "${MOUNT_DIR}"
+  echo ""
 }
 
 function get_osmc_image {
   curl -L "$OSMC_URL" -o "${DOWNLOAD_DIR}/OSMC_${OSMC_VERSION}.img.gz"
+  echo ""
 }
 
 function setup_downloaded_image_as_loopback {
@@ -27,20 +30,24 @@ function setup_downloaded_image_as_loopback {
   sudo losetup -P "${LOOP_DEV}" "${DOWNLOAD_DIR}/OSMC_${OSMC_VERSION}.img"
   sudo mount "${LOOP_DEV}p1" "${MOUNT_DIR}"
   cp -f "${MOUNT_DIR}/filesystem.tar.xz" "${EXTRACT_DIR}"
+  echo ""
 }
 
 function remove_image {
   sudo umount "${MOUNT_DIR}"
   sudo losetup -d "${LOOP_DEV}"
   rm -f "${DOWNLOAD_DIR}/OSMC_${OSMC_VERSION}.img"
+  echo ""
 }
 
 function create_docker {
   cat "${EXTRACT_DIR}/filesystem.tar.xz" | docker import - "mihaigalos/osmc-rpi:base_${OSMC_VERSION}"
+  echo ""
 }
 
 function remove_filesystem {
   rm -f "${EXTRACT_DIR}/filesystem.tar.xz"
+  echo ""
 }
 
 set_variables
